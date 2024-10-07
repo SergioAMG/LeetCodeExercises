5221ffdb878abf58f9364f0b0816edc7a114dba3
sergioarturomartinez <sergioamg@outlook.com>
2024-10-07 09:53:57 -0600

    Solution to Exercise

---
+++ LeetCodeExercises/ArraysAndStrings/IsSubsequence.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercises.ArraysAndStrings
+{
+    /*
+     * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+     */
+    public class IsSubsequence
+    {
+        /// <summary>
+        /// TimeComplexity = O(Max(S,T))
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool IsSubsequenceOf(string s, string t)
+        {
+            char[] sAsChar = s.ToCharArray();
+            char[] tAsChar = t.ToCharArray();
+            int sIndex = 0;
+            int tIndex = 0;
+            bool isSubsequenceOf = false;
+
+            if (tAsChar.Length == 0 && sAsChar.Length == 0)
+            {
+                isSubsequenceOf = true;
+                return isSubsequenceOf;
+            }
+            if (sAsChar.Length == 0 && tAsChar.Length > 0)
+            {
+                isSubsequenceOf = true;
+                return isSubsequenceOf;
+            }
+            if (tAsChar.Length == 0 && sAsChar.Length > 0)
+            {
+                isSubsequenceOf = false;
+                return isSubsequenceOf;
+            }
+            if ((sAsChar.Length == 1 && tAsChar.Length == 1) && sAsChar[sIndex] != tAsChar[tIndex])
+            {
+                isSubsequenceOf = false;
+                return isSubsequenceOf;
+            }
+            if ((sAsChar.Length == 1 && tAsChar.Length == 1) && sAsChar[sIndex] == tAsChar[tIndex])
+            {
+                isSubsequenceOf = true;
+                return isSubsequenceOf;
+            }
+
+            while (sIndex < sAsChar.Length)
+            {
+                if (tIndex == tAsChar.Length)
+                {
+                    isSubsequenceOf = false;
+                    return isSubsequenceOf;
+                }
+
+                if (sAsChar[sIndex] != tAsChar[tIndex])
+                {
+                    tIndex++;
+                    isSubsequenceOf = false;
+                }
+                else
+                {
+                    sIndex++;
+                    tIndex++;
+                    isSubsequenceOf = true;
+                }
+            }
+
+            return isSubsequenceOf;
+        }
+
+        /// <summary>
+        /// TimeComplexity = O(Max(S,T))
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool IsSubsequenceOfString(string s, string t)
+        {
+            int sIndex = 0;
+            int tIndex = 0;
+
+            while (sIndex < s.Length && tIndex < t.Length)
+            {
+                if (s.ElementAt(sIndex) == t.ElementAt(tIndex))
+                    sIndex++;
+
+                tIndex++;
+            }
+
+            return sIndex == s.Length;
+        }
+    }
+}
---
+++ LeetCodeExercisesTests/ArraysAndStrings/IsSubsequenceTests.cs
@@ -0,0 +1,202 @@
+﻿using LeetCodeExercises.ArraysAndStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercisesTests.ArraysAndStrings
+{
+    [TestFixture]
+    public class IsSubsequenceTests
+    {
+        private IsSubsequence isSubsequence = new();
+
+        [Test]
+        public void IsSubsequenceTestOne()
+        {
+            //Arrange
+            string s = "aaaa";
+            string t = "aaaa";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestTwo()
+        {
+            //Arrange
+            string s = "";
+            string t = "";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestThree()
+        {
+            //Arrange
+            string s = "";
+            string t = "abc";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestFour()
+        {
+            //Arrange
+            string s = "abc";
+            string t = "";
+            bool solution = false;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestFive()
+        {
+            //Arrange
+            string s = "a";
+            string t = "a";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestSix()
+        {
+            //Arrange
+            string s = "a";
+            string t = "c";
+            bool solution = false;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestSeven()
+        {
+            //Arrange
+            string s = "abc";
+            string t = "ahbgdc";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestEight()
+        {
+            //Arrange
+            string s = "axc";
+            string t = "ahbgdc";
+            bool solution = false;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestNine()
+        {
+            //Arrange
+            string s = "aukjol";
+            string t = "appurrrrrkjqozl";
+            bool solution = true;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestTen()
+        {
+            //Arrange
+            string s = "acb";
+            string t = "ahbgdc";
+            bool solution = false;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void IsSubsequenceTestEleven()
+        {
+            //Arrange
+            string s = "acb";
+            string t = "ahbcde";
+            bool solution = false;
+
+            //Act
+            bool resultOptimized = isSubsequence.IsSubsequenceOf(s, t);
+            bool result = isSubsequence.IsSubsequenceOfString(s, t);
+
+            //Assert
+            Assert.That(solution, Is.EqualTo(resultOptimized));
+            Assert.That(solution, Is.EqualTo(result));
+        }
+    }
+}
